419d420a3bc405e891a300341b286e05746f3266
Nikola M <nikola.merlic2@gmail.com>
2022-05-31 13:54:42 +0200

    Ne radi background image i import komponente

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="app"></div>
-  <div class="Naslov">
-    <h1>Pet Finder App</h1>
-  </div>
+  <header>
+    <div class="GlavniNaslov">
+      <h1>Pet Finder App</h1>
+    </div>
+  </header>
   <div>
-    <Button />
-  </div>
-
-  <div>
-    <ButtonPronadi />
+    <i>
+      <div>
+        <div id="app"></div>
+        <div>
+          <Button />
+        </div>
+        <div>
+          <ButtonPronadi />
+        </div>
+      </div>
+    </i>
   </div>
 </template>
 
@@ -27,18 +34,24 @@ export default {
         this.$router.push("/prijavanestanka");
       },
     },
-    ButtonPronadi,
   },
 };
 </script>
 
 <style>
-.Naslov {
+.GlavniNaslov {
   background-color: pink;
   padding: 20px;
   margin-bottom: 50px;
   margin-left: 50px;
   margin-right: 50px;
   font-family: fantasy;
+}
+
+i {
+  background-image: url("@/assets/petpics.jpg");
+  background-size: 100%;
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/views/PronadiLjubimca.vue
+++ src/views/PronadiLjubimca.vue
@@ -1,10 +1,18 @@
 <template>
   <p>Nesto</p>
+
+  <div>
+    <Cards />
+  </div>
 </template>
 
 <script>
+//import Cards from "./components/Cards.vue";
 export default {
   name: "PronadiLjubimca",
+  // components: {
+  // Cards,
+  // }
 };
 </script>
 
