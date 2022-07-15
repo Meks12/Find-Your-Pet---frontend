71acd4fc188c5f2ca9c5dd44e89bd2dec21860fc
Meks12 <nikola.merlic2@gmail.com>
2022-07-15 14:45:58 +0200

    Dodano ime i prezime za korisnicke podatke

--- src/views/PodaciKorisnika.vue
+++ src/views/PodaciKorisnika.vue
@@ -1,8 +1,10 @@
 <template>
+<div class = "ImePrez">
   <div class="input-group">
-  <span class="input-group-text">First and last name</span>
-  <input type="text" aria-label="First name" class="form-control">
-  <input type="text" aria-label="Last name" class="form-control">
+  <span class="input-group-text">Ime i prezime</span>
+  <input type="text" aria-label="Last Name" class="form-control">
+  <input type="text" aria-label="First Name" class="form-control">
+</div>
 </div>
 </template>
 
@@ -12,6 +14,16 @@ export default {
 }
 </script>
 
-<style>
+ <style>
+
+.ImePrez{
+  padding: 15px;
+  width: 15cm;
+  margin: 0 auto;
+  float: none;
+  font-family: "Times New Roman", Times, serif;
+  font-size: 1cm;
+}
+
 
 </style>
