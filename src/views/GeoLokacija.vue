a7d1fed76627641a3403e2e72d96098997a0e0e3
Meks12 <nikola.merlic2@gmail.com>
2022-07-18 16:56:03 +0200

    Dodana funkcionalna mapa svijeta

--- src/views/GeoLokacija.vue
+++ src/views/GeoLokacija.vue
@@ -5,12 +5,17 @@
       Latitude: {{currPos.lat.toFixed(2)}}, Longitude: {{currPos.lng.toFixed(2)}}
     </div>
   </div>
+  <div ref="mapDiv" style="width: 100%; height: 80vh" />
 </template>
 
 <script>
 
-import {computed} from 'vue'
+/* eslint-disable no-undef */
+import {computed, ref, onMounted} from 'vue'
 import GeoLocation from '../components/GeoLocation.vue'
+import {Loader} from '@googlemaps/js-api-loader'
+
+const GOOGLE_MAPS_API_KEY = 'AIzaSyCh-kFS3bfWJBkAlSFrcWUoCesLXZaI9_A'
 
 export default {
   name: 'GeoLokacija',
@@ -20,7 +25,16 @@ export default {
       lat: coords.value.latitude,
       lng: coords.value.longitude
     }))
-    return {currPos}
+
+    const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY})
+    const mapDiv = ref(null)
+    onMounted(async () =>{
+      await loader.load()
+      new google.maps.Map(mapDiv.value, {
+        center: currPos.value, zoom: 7
+      }) 
+    })
+    return {currPos, mapDiv}
   }
 }
 </script>
