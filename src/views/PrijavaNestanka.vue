fbbeda7ee08777eae7f98cfb0e76f7e9214ad359
Meks12 <nikola.merlic2@gmail.com>
2022-07-20 11:39:32 +0200

    Novi pokusaj spremanja slike

--- src/views/PrijavaNestanka.vue
+++ src/views/PrijavaNestanka.vue
@@ -38,6 +38,17 @@
         aria-describedby="basic-addon1"
       />
     </div>
+
+    <div>
+      <div
+        class="imagePreviewWrapper"
+        :style="{ 'background-image': `url(${previewImage})` }"
+        @click="selectImage"
+      ></div>
+
+      <input ref="fileInput" type="file" @input="pickFile" />
+    </div>
+
     <div class="SlikaPsa">
       <h3>
         <span class="badge bg-secondary">Odaberi sliku psa</span>
@@ -110,7 +121,7 @@ export default {
       stanje: "",
       geo_lokacija: "",
       vrsta_psa: "",
-      slika: "",
+      previewImage: null,
       cip: "",
       spol: "",
       datum_nestanka: "",
@@ -146,6 +157,21 @@ export default {
       };
       console.log(podaci);
       xhr.send(JSON.stringify(podaci));
+    },
+    selectImage() {
+      this.$refs.fileInput.click();
+    },
+    pickFile() {
+      let input = this.$refs.fileInput;
+      let file = input.files;
+      if (file && file[0]) {
+        let reader = new FileReader();
+        reader.onload = (e) => {
+          this.previewImage = e.target.result;
+        };
+        reader.readAsDataURL(file[0]);
+        this.$emit("input", file[0]);
+      }
     },
   },
 };
