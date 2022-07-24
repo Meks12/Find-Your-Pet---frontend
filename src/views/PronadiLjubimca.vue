fc31a8307bffa76f766d8c9e5f526eea2c7529a2
Meks12 <nikola.merlic2@gmail.com>
2022-07-24 19:19:00 +0200

    Proba dodavanja vlasnikovih inf u card

--- src/views/PronadiLjubimca.vue
+++ src/views/PronadiLjubimca.vue
@@ -7,21 +7,55 @@
     {{ ljubimac.ime }}
   </div>
 
-  <div v-for="ljubimac in Ljubimci" :key="ljubimac">
-    <div class="card" style="width: 30rem">
-      <img class="card-img-top" alt="Card image cap" />
-      <div class="card-body">
-        <h5 class="card-title">Ime izgubljenog psa: {{ ljubimac.ime }}</h5>
-        <p class="card-text">Informacije o izgubljenom ljubimcu</p>
+  <div class="d-flex justify content-start">
+    <div v-for="ljubimac in Ljubimci" :key="ljubimac">
+      <div class="card" style="width: 30rem">
+        <img class="card-img-top" alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title">Ime izgubljenog psa: {{ ljubimac.ime }}</h5>
+          <p class="card-text">Informacije o izgubljenom ljubimcu</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            Vrsta ljubimca: {{ ljubimac.vrsta_psa }}
+          </li>
+          <li class="list-group-item">
+            Stanje u kojem je pas zadnji put viđen: {{ ljubimac.stanje }}
+          </li>
+          <li class="list-group-item">Status čipiranja: {{ ljubimac.cip }}</li>
+          <li class="list-group-item">Spol: {{ ljubimac.spol }}</li>
+          <li class="list-group-item">
+            Datum nestanka: {{ ljubimac.datum_nestanka }}
+            <div class="accordion" id="accordionExample">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                  <button
+                    class="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    Detalji vlasnika
+                  </button>
+                </h2>
+                <div
+                  id="collapseOne"
+                  class="accordion-collapse collapse show"
+                  aria-labelledby="headingOne"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div class="accordion-body">
+                    {{ ljubimac.ime }}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <div class="card-body"></div>
       </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">{{ ljubimac.vrsta_psa }}</li>
-        <li class="list-group-item">{{ ljubimac.stanje }}</li>
-        <li class="list-group-item">{{ ljubimac.cip }}</li>
-        <li class="list-group-item">{{ ljubimac.spol }}</li>
-        <li class="list-group-item">{{ ljubimac.datum_nestanka }}</li>
-      </ul>
-      <div class="card-body"></div>
     </div>
   </div>
 </template>
@@ -79,5 +113,11 @@ export default {
 }
 .left-two {
   object-position: left;
+}
+
+.card {
+  font-family: "Times New Roman", Times, serif;
+  font-size: 15px;
+  margin: 20px;
 }
 </style>
